b75af40872029e578e6ad1d6079b910d50ebb2c9
Mihai <amihai@optaros.com>
2018-06-08 18:18:22 +0300

    added methods to read and save pdf

--- src/main/java/com/ncr/pages/PDFPageObject.java
+++ src/main/java/com/ncr/pages/PDFPageObject.java
@@ -2,13 +2,21 @@ package com.ncr.pages;
 
 import com.ncr.base.BasePageObject;
 import org.apache.pdfbox.cos.COSDocument;
+import org.apache.pdfbox.pdfparser.PDFParser;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 
-import java.io.IOException;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class PDFPageObject extends BasePageObject<PDFPageObject> {
     private By textLayer = By.cssSelector("div.page:nth-child(1) > div:nth-child(2)");
@@ -18,11 +26,37 @@ public class PDFPageObject extends BasePageObject<PDFPageObject> {
         super(driver);
     }
 
-    public boolean checkPDFContent(String url, String text){
-        boolean flag = false;
-        PDFTextStripper pdfStripper = null;
-        PDDocument pdDoc = null;
-        COSDocument cosDoc = null;
-        String parsedText = null;
+//    File lastFile = lastModified("D:\\PDF\\");
+
+    public void checkPDFContent(File file) throws IOException {
+
+        try{
+            PDDocument doc = PDDocument.load(file);
+            int totalPages = doc.getNumberOfPages();
+            System.out.println("Total pages: " + totalPages);
+            PDFTextStripper stripper = new PDFTextStripper();
+            stripper.setStartPage(1);
+            stripper.setEndPage(totalPages);
+            String extractedText = stripper.getText(doc);
+            System.out.println(extractedText);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void savePDF(String url) throws IOException {
+        byte[] ba1 = new byte[1024];
+        URL url1 = new URL(url);
+        int baLength;
+        String date = new SimpleDateFormat("dd.MM.yyyy_HH.mm.ss").format(new Date());
+        FileOutputStream pdf1 = new FileOutputStream("regimen" + date +".pdf");
+
+        InputStream in1 = url1.openStream();
+        while((baLength = in1.read(ba1)) != -1){
+            pdf1.write(ba1, 0, baLength);
+        }
+        pdf1.flush();
+        pdf1.close();
+        in1.close();
     }
 }
--- src/test/java/PrintRegimenTest.java
+++ src/test/java/PrintRegimenTest.java
@@ -2,18 +2,23 @@ import com.ncr.pages.PDFPageObject;
 import com.ncr.pages.RegimenSavedPageObject;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.io.IOException;
 
 public class PrintRegimenTest extends CreateRegimenTest {
     @Test(priority=4)
-    public void PrintRegimenTest() throws IOException {
+    public void PrintRegimenTest() throws IOException, InterruptedException {
         RegimenSavedPageObject regimenSavedPage = new RegimenSavedPageObject(driver);
         regimenSavedPage.waitForPageToLoad();
         PDFPageObject pdfPage = regimenSavedPage.print();
+        Thread.sleep(9000);
         //Switch to new tab
         for(String winHandle : driver.getWindowHandles()){
             driver.switchTo().window(winHandle);
         }
-        pdfPage.
+        String currentURL = driver.getCurrentUrl();
+        pdfPage.savePDF(currentURL);
+        File lastFile = pdfPage.lastModified("D:\\PDF\\");
+        pdfPage.checkPDFContent(lastFile);
     }
 }
