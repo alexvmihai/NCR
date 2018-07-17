7c239b7034ced36b02d7dd33b69e31ff7693b64d
Mihai <amihai@optaros.com>
2018-07-17 17:43:16 +0300

    added multiple selectors on the Nutrition page

--- src/main/java/com/ncr/pages/NutritionProductsPageObject.java
+++ src/main/java/com/ncr/pages/NutritionProductsPageObject.java
@@ -21,6 +21,20 @@ public class NutritionProductsPageObject extends BasePageObject<NutritionProduct
     private By tube = By.cssSelector("div.block:nth-child(1) > div:nth-child(2) > ol:nth-child(1) > li:nth-child(6) > a:nth-child(1)");
     private By consumables = By.cssSelector("div.block:nth-child(1) > div:nth-child(2) > ol:nth-child(1) > li:nth-child(7) > a:nth-child(1)");
     private By gut = By.cssSelector("li.active:nth-child(8) > a:nth-child(1)");
+    //Filters
+    private By view = By.cssSelector("div.toolbar:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > select:nth-child(1)");
+    private By position = By.cssSelector("div.toolbar:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > select:nth-child(1)");
+    private By viewAll = By.cssSelector("div.toolbar:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > a:nth-child(3)");
+    private By flavour = By.cssSelector("#narrow-by-list > dt:nth-child(1)");
+    private By apple = By.cssSelector("dd.sln-type-checkbox:nth-child(2) > ol:nth-child(1) > li:nth-child(1) > a:nth-child(1)");
+    private By apple_prune = By.cssSelector("dd.sln-type-checkbox:nth-child(2) > ol:nth-child(1) > li:nth-child(2) > a:nth-child(1)");
+    private By apple_straw = By.cssSelector("dd.sln-type-checkbox:nth-child(2) > ol:nth-child(1) > li:nth-child(3) > a:nth-child(1)");
+    private By neutral = By.cssSelector("dd.sln-type-checkbox:nth-child(2) > ol:nth-child(1) > li:nth-child(4) > a:nth-child(1)");
+    private By orange = By.cssSelector("dd.sln-type-checkbox:nth-child(2) > ol:nth-child(1) > li:nth-child(5) > a:nth-child(1)");
+    private By unit = By.cssSelector("dt.active:nth-child(3) > span:nth-child(1)");
+    private By caseF = By.cssSelector("dd.sln-type-checkbox:nth-child(4) > ol:nth-child(1) > li:nth-child(1) > a:nth-child(1)");
+    private By each = By.cssSelector("dd.sln-type-checkbox:nth-child(4) > ol:nth-child(1) > li:nth-child(2) > a:nth-child(1)");
+
 
 
     protected NutritionProductsPageObject(WebDriver driver) throws IOException {
