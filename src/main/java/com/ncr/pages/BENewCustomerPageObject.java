51daa3b6d9b21ffcbe5f0680a0ee9873a1f2275a
Mihai <amihai@optaros.com>
2018-09-03 17:52:16 +0300

    added functions, pages

--- src/main/java/com/ncr/pages/BENewCustomerPageObject.java
+++ src/main/java/com/ncr/pages/BENewCustomerPageObject.java
@@ -1,15 +1,61 @@
 package com.ncr.pages;
 
 import com.ncr.base.BasePageObject;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
 
 import java.io.IOException;
 
 public class BENewCustomerPageObject extends BasePageObject<BENewCustomerPageObject> {
+    private By header = By.cssSelector("div.content-header:nth-child(2) > h3:nth-child(1)");
+    private By websiteF = By.cssSelector("#_accountwebsite_id");
+    private By customerTypeF = By.cssSelector("#_accountcustomer_type");
+    private By institutionF = By.cssSelector("#_accountinstitution_id");
+    private By firstNameF = By.cssSelector("#_accountfirstname");
+    private By lastNameF = By.cssSelector("#_accountlastname");
+    private By emailF = By.cssSelector("#_accountemail");
+    private By status = By.cssSelector("#_accountstatus");
+    private By passwordF = By.cssSelector("#_accountpassword");
+    private By saveContinueB = By.cssSelector("div.content-header:nth-child(2) > p:nth-child(2) > button:nth-child(4)");
+    private By saveCustomerB = By.cssSelector("div.content-header:nth-child(2) > p:nth-child(2) > button:nth-child(3)");
 
 
 
     protected BENewCustomerPageObject(WebDriver driver) throws IOException {
         super(driver);
     }
+
+    public void fillForm (String site, String customer, String hospital, String firstname, String lastname, String email, String password){
+        Select website = new Select(driver.findElement(By.cssSelector("#_accountwebsite_id")));
+        website.selectByVisibleText(site);
+        Select type = new Select(driver.findElement(By.cssSelector("#_accountcustomer_type")));
+        type.selectByVisibleText(customer);
+        Select institution = new Select(driver.findElement(By.cssSelector("#_accountinstitution_id")));
+        institution.selectByVisibleText(hospital);
+        type(firstname, firstNameF);
+        type(lastname, lastNameF);
+        type(email, emailF);
+        Select status = new Select(driver.findElement(By.cssSelector("#_accountstatus")));
+        status.selectByVisibleText("Active");
+        type(password, passwordF);
+
+    }
+
+    public void waitForPageToLoad(){
+        waitForVisibilityOf(header);
+        waitForVisibilityOf(websiteF);
+        waitForVisibilityOf(saveContinueB);
+    }
+
+    public String getHeaderText(){
+        return getText(header);
+    }
+
+    public BECustomerInfoPageObject saveContinue() throws IOException{
+        clickOn(saveContinueB);
+        return new BECustomerInfoPageObject(driver);
+    }
+
+
 }
--- src/test/java/BEHCPRegisterTest.java
+++ src/test/java/BEHCPRegisterTest.java
@@ -1,7 +1,6 @@
 import com.ncr.base.BaseTest;
-import com.ncr.pages.BEManageCustomersPageObject;
-import com.ncr.pages.BackEndDashboardPageObject;
-import com.ncr.pages.BackEndLoginPageObject;
+import com.ncr.pages.*;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -25,6 +24,24 @@ public class BEHCPRegisterTest extends BaseTest {
         dashboard.mouseOverCustomers();
         BEManageCustomersPageObject manageCustomers = dashboard.clickManageCustomers();
         manageCustomers.waitForPageToLoad();
+        BENewCustomerPageObject newHCP = manageCustomers.addCustomer();
+        newHCP.waitForPageToLoad();
+        String expectedHeader = "New Customer";
+        String actualHeader = newHCP.getHeaderText();
+        Assert.assertTrue(expectedHeader.equals(actualHeader), "The header is not correct !" + "\nExpected: " + expectedHeader + "\nActual: " + actualHeader);
+        long Random = Math.round(Math.random() * 1357987.0D);
+        String email = "alex.mihai.optaros+" + Random + "@gmail.com";
+        String firstname = "Auto_Firstname" + Random;
+        String lastname = "Auto_Lastname" + Random;
+        String password = "Ncare1234/";
+        newHCP.fillForm("Admin", "HCP", "NCare Test Hospital", firstname, lastname, email, password);
+        BECustomerInfoPageObject infoPage = newHCP.saveContinue();
+        infoPage.waitForPageToLoad();
+        String expectedSaveMsg = "The customer has been saved.";
+        String actualSaveMsg = infoPage.getSuccessMessage();
+        Assert.assertTrue(expectedSaveMsg.equals(actualSaveMsg), "Success Msg not correct !" + "\nExpected: " + expectedSaveMsg + "\nActual: " + actualSaveMsg);
+
+
 
     }
 }
