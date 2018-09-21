55d9b38bb146bbc25e0b5dbe9b551e8352f3d232
Alex V Mihai <alexvmihai>
2018-09-21 11:58:00 +0300

    crated class for searchpatient

---
+++ src/test/java/SearchPatientTest.java
@@ -0,0 +1,9 @@
+import com.ncr.base.BaseTest;
+import org.testng.annotations.Test;
+
+public class SearchPatientTest extends BaseTest {
+    @Test
+    public static void searchPatient(){
+
+    }
+}
